2f1934e4d08b6f30df25d250ea69d9730c5e3c7a
IsaqueCeo <isaqueabraaodesousa0000@gmail.com>
2024-07-04 20:21:58 -0300

    Perfil

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,75 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home Screen</Text>
-      <Text>This is the main screen with some example texts.</Text>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Feather name="menu" size={24} color="#fff" />
+        </TouchableOpacity>
+        <Text style={styles.searchTitle}>Pesquisa</Text>
+        <TouchableOpacity>
+          <Feather name="chevron-down" size={24} color="#fff" />
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.searchBar}>
+        <Feather name="search" size={24} color="#6D4AFF" />
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Digite sua pesquisa"
+          placeholderTextColor="#6D4AFF"
+        />
+      </View>
+
+      <View style={styles.iconContainer}>
+        <TouchableOpacity style={styles.icon}>
+          <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/201/201426.png' }} style={styles.iconImage} />
+          <Text style={styles.iconText}>Hospedagem</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.icon}>
+          <Image source={{ uri: 'https://cdn.icon-icons.com/icons2/38/PNG/512/bustransport_bus_4984.png' }} style={styles.iconImage} />
+          <Text style={styles.iconText}>Transporte</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.icon}>
+          <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/1085/1085493.png' }} style={styles.iconImage} />
+          <Text style={styles.iconText}>Voos</Text>
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView style={styles.recommendations}>
+        <Text style={styles.recommendationsTitle}>Recomendações</Text>
+
+        <View style={styles.recommendationCard}>
+          <Image source={{ uri: 'https://media.gettyimages.com/id/638921947/pt/foto/mexico-city-mexico.jpg?s=612x612&w=gi&k=20&c=UmYU2vMJPy3guRnEgobDIwZLT2vgChywsQxwyCOqk30=' }} style={styles.recommendationImage} />
+          <View style={styles.recommendationContent}>
+            <Text style={styles.recommendationTop}>Destino</Text>
+            <Text style={styles.recommendationName}>Nome do lugar</Text>
+            <Text style={styles.recommendationDescription}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in cursus magna.
+            </Text>
+            <View style={styles.recommendationFooter}>
+              <Text style={styles.recommendationDiscount}>-30%</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.recommendationCard}>
+          <Image source={{ uri: 'https://blog.123milhas.com/wp-content/uploads/2022/09/estados-unidos-guia-de-turismo-cultura-gastronomia-e-mais-walt-disney-world-conexao123.jpg' }} style={styles.recommendationImage} />
+          <View style={styles.recommendationContent}>
+            <Text style={styles.recommendationTop}>Destino</Text>
+            <Text style={styles.recommendationName}>Nome do lugar</Text>
+            <Text style={styles.recommendationDescription}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in cursus magna.
+            </Text>
+            <View style={styles.recommendationFooter}>
+              <Text style={styles.recommendationDiscount}>-25%</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,12 +77,97 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
+    backgroundColor: '#fff',
   },
-  title: {
-    fontSize: 24,
+  header: {
+    backgroundColor: '#6D4AFF',
+    paddingTop: 50,
+    paddingBottom: 20,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  searchTitle: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  searchBar: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    borderRadius: 30,
+    padding: 10,
+    margin: 20,
+    alignItems: 'center',
+  },
+  searchInput: {
+    flex: 1,
+    marginLeft: 10,
+    color: '#6D4AFF',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 20,
+  },
+  icon: {
+    alignItems: 'center',
+  },
+  iconImage: {
+    width: 50,
+    height: 50,
+    marginBottom: 5,
+  },
+  iconText: {
+    color: '#6D4AFF',
+    fontWeight: 'bold',
+  },
+  recommendations: {
+    paddingHorizontal: 20,
+  },
+  recommendationsTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#6D4AFF',
+    marginBottom: 10,
+  },
+  recommendationCard: {
+    backgroundColor: '#f8f8f8',
+    borderRadius: 10,
+    overflow: 'hidden',
     marginBottom: 20,
+  },
+  recommendationImage: {
+    width: '100%',
+    height: 150,
+  },
+  recommendationContent: {
+    padding: 10,
+  },
+  recommendationTop: {
+    color: '#6D4AFF',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  recommendationName: {
+    color: '#333',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  recommendationDescription: {
+    color: '#666',
+    fontSize: 14,
+  },
+  recommendationFooter: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 10,
+  },
+  recommendationDiscount: {
+    color: '#6D4AFF',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,55 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Feather, FontAwesome } from '@expo/vector-icons';
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Profile Screen</Text>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Feather name="arrow-left" size={24} color="#fff" />
+        </TouchableOpacity>
+        <Text style={styles.headerText}>Perfil</Text>
+        <TouchableOpacity>
+          <Feather name="settings" size={24} color="#fff" />
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.profileContainer}>
+        <Image
+          source={{ uri: 'https://i0.statig.com.br/bancodeimagens/2f/ym/i8/2fymi85z5vo5pcl5rsnsr3xgi.jpg' }}
+          style={styles.profileImage}
+        />
+        <Text style={styles.profileName}>Isaque Abraão</Text>
+        <Text style={styles.profileJob}>Programador Full Stack</Text>
+        <View style={styles.followContainer}>
+          <Text style={styles.followers}>1000 Seguidores</Text>
+          <Text style={styles.following}>1200 Seguidores</Text>
+        </View>
+      </View>
+      
+      <View style={styles.contactContainer}>
+        <View style={styles.contactItem}>
+          <Feather name="mail" size={20} color="#888" />
+          <Text style={styles.contactText}>isaque@gmail.com</Text>
+        </View>
+        <View style={styles.contactItem}>
+          <Feather name="phone" size={20} color="#888" />
+          <Text style={styles.contactText}>1234567891</Text>
+        </View>
+        <View style={styles.contactItem}>
+          <FontAwesome name="twitter" size={20} color="#888" />
+          <Text style={styles.contactText}>@Isaque012</Text>
+        </View>
+        <View style={styles.contactItem}>
+          <Feather name="link" size={20} color="#888" />
+          <Text style={styles.contactText}>www.behance.net/Isaque012</Text>
+        </View>
+        <View style={styles.contactItem}>
+          <FontAwesome name="facebook" size={20} color="#888" />
+          <Text style={styles.contactText}>www.facebook.com/Isaque012</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,12 +57,71 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#f2f2f2',
+  },
+  header: {
+    backgroundColor: '#6D4AFF',
+    paddingTop: 50,
+    paddingBottom: 20,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  profileContainer: {
+    backgroundColor: '#6D4AFF',
+    padding: 20,
+    alignItems: 'center',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: '#fff',
+  },
+  profileName: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  profileJob: {
+    color: '#fff',
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  followContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  followers: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  following: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  contactContainer: {
     padding: 20,
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
+  contactItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  contactText: {
+    marginLeft: 10,
+    fontSize: 16,
+    color: '#333',
   },
 });
